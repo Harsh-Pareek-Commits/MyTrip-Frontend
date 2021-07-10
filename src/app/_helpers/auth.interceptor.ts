12d3aee9d69dfed0b449237676a17e985db6dddd
Harsh Pareek <32735763+Harsh-Pareek-Commits@users.noreply.github.com>
2021-07-10 11:13:44 +0530

    Latest hwt working

---
+++ src/app/_helpers/auth.interceptor.ts
@@ -0,0 +1,24 @@
+import { HTTP_INTERCEPTORS, HttpEvent } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor() { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (sessionStorage.getItem('token') && sessionStorage.getItem('userId')) {
+      console.log("Interceptor called with "+ sessionStorage.getItem('token'));
+      request = request.clone({ headers: request.headers.set('Authorization', 'Bearer ' + sessionStorage.getItem('token')) })
+    }
+   
+  return next.handle(request);
+}
+}
+
+export const authInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+];
