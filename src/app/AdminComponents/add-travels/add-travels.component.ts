c39c2026c13984f7931918812abeffe33ed46167
jaymaity123 <jaydeepmaity123@gmail.com>
2021-07-16 23:33:50 +0530

    jay

--- src/app/AdminComponents/add-travels/add-travels.component.ts
+++ src/app/AdminComponents/add-travels/add-travels.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup,FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import {faGooglePlusG} from '@fortawesome/free-brands-svg-icons';
+import { ToastrService } from 'ngx-toastr';
+import { AuthServiceService } from 'src/app/Services/auth-service.service';
 @Component({
   selector: 'app-add-travels',
   templateUrl: './add-travels.component.html',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTravelsComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  travelForm!: FormGroup;
+  faGoogle=faGooglePlusG;
+  submitted = false;
+  constructor(private authService: AuthServiceService,private formBuilder:FormBuilder,private toastr: ToastrService, private router: Router) { }
+   ngOnInit(): void {
+    sessionStorage.clear();
+    console.log("Local cleared");
+    this.initForm();
+     
+    }
+  initForm(){
+    this.travelForm=this.formBuilder.group({
+      travelsName:["",[Validators.required]],
+      agentName:["",[Validators.required]],
+      agentAddress:["",[Validators.required]],
+      agentContact:["",[Validators.required]],
+    })
   }
+  get control(){
+
+return this.travelForm.controls;
+  }
+  addTravel()
+{
+  this.submitted=true;
+  
+  if(this.travelForm.valid){
+
+  }
+}
 
 }
