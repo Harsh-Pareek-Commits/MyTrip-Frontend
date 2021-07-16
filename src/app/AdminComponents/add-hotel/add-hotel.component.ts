c5a06522fd56d340ca17112665fecdb0a2f89931
Iflak Yousuf Mir <iflak98@gmail.com>
2021-07-16 23:25:37 +0530

    iff

--- src/app/AdminComponents/add-hotel/add-hotel.component.ts
+++ src/app/AdminComponents/add-hotel/add-hotel.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup,FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import {faGooglePlusG} from '@fortawesome/free-brands-svg-icons';
+import { ToastrService } from 'ngx-toastr';
+import { AuthServiceService } from 'src/app/Services/auth-service.service';
 @Component({
   selector: 'app-add-hotel',
   templateUrl: './add-hotel.component.html',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddHotelComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  hotelForm!: FormGroup;
+  faGoogle=faGooglePlusG;
+  submitted = false;
+  constructor(private authService: AuthServiceService,private formBuilder:FormBuilder,private toastr: ToastrService, private router: Router) { }
+   ngOnInit(): void {
+    sessionStorage.clear();
+    console.log("Local cleared");
+    this.initForm();
+     
+    }
+  initForm(){
+    this.hotelForm=this.formBuilder.group({
+      hotelName:["",[Validators.required]],
+      hotelType:["",[Validators.required]],
+      hotelDescription:["",[Validators.required]],
+      hotelAddress:["",[Validators.required]],
+      hotelRent:["",[Validators.required]],
+      hotelStatus:["",[Validators.required]],
+      hotelCity:["",[Validators.required]],
+    })
   }
+  get control(){
 
+return this.hotelForm.controls;
+  }
+  addHotel()
+{
+  this.submitted=true;
+  
+  if(this.hotelForm.valid){
+
+  }
+}
 }
--- src/app/AdminComponents/add-route/add-route.component.ts
+++ src/app/AdminComponents/add-route/add-route.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup,FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import {faGooglePlusG} from '@fortawesome/free-brands-svg-icons';
+import { ToastrService } from 'ngx-toastr';
+import { AuthServiceService } from 'src/app/Services/auth-service.service';
 @Component({
   selector: 'app-add-route',
   templateUrl: './add-route.component.html',
   styleUrls: ['./add-route.component.css']
 })
 export class AddRouteComponent implements OnInit {
+  addRouteForm!: FormGroup;
+  faGoogle=faGooglePlusG;
+  submitted = false;
+  constructor(private authService: AuthServiceService,private formBuilder:FormBuilder,private toastr: ToastrService, private router: Router) { }
+   ngOnInit(): void {
+    this.addRouteForm=this.formBuilder.group({
+      routeFrom:["",[Validators.required]],
+      routeTo:["",[Validators.required]],
+      deptDate:["",[Validators.required]],
+      arrivalDate:["",[Validators.required]],
+      deptTime:["",[Validators.required]],
+      arrivalTime:["",[Validators.required]],
+      pickUpPoint:["",[Validators.required]],
+      fare:["",[Validators.required]],
 
-  constructor() { }
+      busType:["",[Validators.required]],
+      busNumber:["",[Validators.required]],
+      busCapacity:["",[Validators.required]],
+      travel:["",[Validators.required]],
 
-  ngOnInit(): void {
+    })
   }
+  get control(){
 
+return this.addRouteForm.controls;
+  }
+  addRoute()
+  {
+    this.submitted=true;
+    
+    if(this.addRouteForm.valid){
+  
+    }
+  }
 }
