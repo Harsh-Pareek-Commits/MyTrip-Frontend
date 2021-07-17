87d7c4b5d08daa46ad538b2cd73c40c07d289498
Harsh Pareek <32735763+Harsh-Pareek-Commits@users.noreply.github.com>
2021-07-17 12:41:16 +0530

    add travels and add admin working

--- src/app/EntityDtoModels/admin-entity-dto.ts
+++ src/app/EntityDtoModels/admin-entity-dto.ts
@@ -1,10 +1,22 @@
 export class AdminEntityDto {
+    userId:number;
+    userEmail:string;
+    password:string;
     name:string;
     adminMobile:number;
 
-  constructor(name: string, adminMobile: number) {
+  constructor(
+    userId: number, 
+    userEmail: string, 
+    password: string, 
+    name: string, 
+    adminMobile: number
+) {
+    this.userId = userId
+    this.userEmail = userEmail
+    this.password = password
     this.name = name
     this.adminMobile = adminMobile
   }
-
+  
 }
