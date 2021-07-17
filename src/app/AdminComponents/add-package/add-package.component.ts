d0473286cad368fd31fc200acf3ffd004dc350a9
jaymaity123 <jaydeepmaity123@gmail.com>
2021-07-18 01:01:06 +0530

    package,route

--- src/app/AdminComponents/add-package/add-package.component.ts
+++ src/app/AdminComponents/add-package/add-package.component.ts
@@ -1,3 +1,6 @@
+import { Route } from 'src/app/Models/route';
+import { PackageService } from 'src/app/Services/package.service';
+import { Package } from 'src/app/Models/package';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup,FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -13,8 +16,17 @@ export class AddPackageComponent implements OnInit {
   addPackageForm!: FormGroup;
   faGoogle=faGooglePlusG;
   submitted = false;
-  constructor(private authService: AuthServiceService,private formBuilder:FormBuilder,private toastr: ToastrService, private router: Router) { }
-   ngOnInit(): void {
+  listPackage!:Package[];
+  listRoute!:Route[];
+  constructor(private packageservice:PackageService,private formBuilder:FormBuilder,private toastr: ToastrService, private router: Router) { }
+
+    ngOnInit(): void {
+
+      this.initForm();
+      this.getPackage();
+      this.viewRoute();
+      }
+      initForm(){
     this.addPackageForm=this.formBuilder.group({
       packName:["",[Validators.required]],
       packDesc:["",[Validators.required]],
@@ -37,4 +49,42 @@ return this.addPackageForm.controls;
   
     }
   }
+  getPackage(){
+    this.packageservice.getAllPack().subscribe(data=>{
+    this.listPackage=data;
+    },(error)=> {
+      if (error.status === 404) {
+        this.toastr.info("No Package found! Try again")
+        this.router.navigate(['/admin/hotel'])
+      } else if (error.status === 403) {
+        this.toastr.error("Please login first!")
+        this.router.navigate(['/login'])
+      }
+      else {
+        console.log(error);
+        this.router.navigate(['/admin/dashboard'])
+        this.toastr.error("Something went wrong")
+      }
+    })
+  }
+  viewRoute()
+  {
+    this.packageservice.viewRoute().subscribe(data=>{
+    this.listRoute=data;
+    },(error)=> {
+      if (error.status === 404) {
+        this.toastr.info("No Route found! Try again")
+        this.router.navigate(['/admin/travels'])
+      } else if (error.status === 403) {
+        this.toastr.error("Please login first!")
+        this.router.navigate(['/login'])
+      }
+      else {
+        console.log(error);
+        this.router.navigate(['/admin/dashboard'])
+        this.toastr.error("Something went wrong")
+      }
+    })
+  }
+
 }
