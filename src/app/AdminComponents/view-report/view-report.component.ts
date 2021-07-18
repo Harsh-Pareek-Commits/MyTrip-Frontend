21bcfa31484e7a4796317b1378972594f78cfa32
jaymaity123 <jaydeepmaity123@gmail.com>
2021-07-18 23:57:33 +0530

    report

--- src/app/AdminComponents/view-report/view-report.component.ts
+++ src/app/AdminComponents/view-report/view-report.component.ts
@@ -17,8 +17,28 @@ export class ViewReportComponent implements OnInit {
   submitted = false;
   Report!:any;
   listReport!:Report[];
+
   constructor(private reportservice:ReportService,private formBuilder:FormBuilder,private toastr: ToastrService, private router: Router) { }
   ngOnInit(): void {
-
+this.viewReport();
+  }
+  viewReport()
+  {
+    this.reportservice.viewReport().subscribe(data=>{
+    this.listReport=data;
+    },(error)=> {
+      if (error.staus = 404) {
+        this.toastr.info("No Report found! Try again")
+        this.router.navigate(['/admin/report'])
+      } else if (error.staus = 403) {
+        this.toastr.error("Please login first!")
+        this.router.navigate(['/login'])
+      }
+      else {
+        console.log(error);
+        this.router.navigate(['/admin/dashboard'])
+        this.toastr.error("Something went wrong")
+      }
+    })
   }
 }
