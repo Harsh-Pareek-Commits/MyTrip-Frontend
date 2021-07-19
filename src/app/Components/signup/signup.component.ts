8e835f77b42dd9dcde62b2c747a0da5b2643ccb2
Iflak Yousuf Mir <iflak98@gmail.com>
2021-07-20 00:49:23 +0530

    signup,feedback done

--- src/app/Components/signup/signup.component.ts
+++ src/app/Components/signup/signup.component.ts
@@ -3,7 +3,10 @@ import { FormControl, FormGroup,FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import {faGooglePlusG} from '@fortawesome/free-brands-svg-icons';
 import { ToastrService } from 'ngx-toastr';
+import { ConfirmedValidator } from 'src/app/confirmed.validator';
+import { CustomerEntityDto } from 'src/app/EntityDtoModels/customer-entity-dto';
 import { AuthServiceService } from 'src/app/Services/auth-service.service';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-signup',
@@ -11,49 +14,55 @@ import { AuthServiceService } from 'src/app/Services/auth-service.service';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  loginForm!: FormGroup;
+  signupForm!: FormGroup;
   faGoogle=faGooglePlusG;
+ cust!:any;
   submitted = false;
-  constructor(private authService: AuthServiceService,private formBuilder:FormBuilder,private toastr: ToastrService, private router: Router) { }
+  
+  constructor(private userService: UserService,private formBuilder:FormBuilder,private toastr: ToastrService, private router: Router) { }
    ngOnInit(): void {
-    sessionStorage.clear();
-    console.log("Local cleared");
+  
     this.initForm();
+    this.signUp();
      
     }
   initForm(){
-    this.loginForm=this.formBuilder.group({
-      email:["",[Validators.required]],
-      password:["",[Validators.required]]
+    this.signupForm=this.formBuilder.group({
+      custName:["",[Validators.required]],
+      userEmail:["",[Validators.required]],
+      password:["",[Validators.required]],
+  confirmpassword:["",[Validators.required]],
+      custMobileNo:["",[Validators.required]],
+      custAddress:["",[Validators.required]]
 
-    })
+    }, { 
+      validator: ConfirmedValidator('password', 'confirmpassword')})
   }
   get control(){
 
-return this.loginForm.controls;
+return this.signupForm.controls;
   }
-  loginProcess(){
-    this.submitted=true;
-    console.log("LOGIN PROCESS");
-    if(this.loginForm.valid){
-      this.authService.login(this.loginForm.value).subscribe(
-        result=>{
-         
-          sessionStorage.setItem('token',result.token);
-          sessionStorage.setItem('userId',result.id);
-         
-          this.authService.loggedIn.next(true)
-          this.toastr.success ('Login Success');
-          this.router.navigate(['/home']);
-          
 
-      }, (error) => { 
-        console.log(error)
-        this.toastr.error ('Invalid Cridential');
-        
-      }
-      
-      )
-    }
+signUp()
+{
+  this.submitted=true;
+  
+  if(this.signupForm.valid){
+     var customer =new CustomerEntityDto("0",this.signupForm.get('custName')?.value,this.signupForm.get('custAddress')?.value,this.signupForm.get('custMobileNo')?.value,"3",this.signupForm.get('userEmail')?.value,this.signupForm.get('password')?.value);
+    
+     this.userService.addcustomer(customer).subscribe(data=>{
+      this.cust=data;
+      this.toastr.success("SignUp Success")
+      this.router.navigate(['/login'])
+      .then(() => {
+        window.location.reload();
+      });
+    },(error)=>{
+      console.log(error);
+      this.toastr.error("Somthing Went Wrong. Please try again")
+
+    })
+    
   }
+}
 }
