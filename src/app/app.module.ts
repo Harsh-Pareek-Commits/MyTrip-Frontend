d8e31b210c592557f2ea9d23ac94f199d735477e
Iflak Yousuf Mir <iflak98@gmail.com>
2021-07-20 09:53:46 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { ReportComponent } from './Components/report/report.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule ,appRoutes} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
 import { HeaderComponent } from './Components/header/header.component';
